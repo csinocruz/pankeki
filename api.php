6fa09649584575acae128d5516a7e04ea195c7d9
Cesca <csinocruz@gmail.com>
2018-06-08 22:00:17 -0700

    post data button is working, ajax request data button's php is not setup correctly

--- api.php
+++ api.php
@@ -1,31 +1,64 @@
 <?php
-    $con = new mysqli('localhost', 'root', 'root', 'pancake-app');
-    $arr = [];
+$connection = new mysqli('localhost', 'root', 'root', 'pancake-app');
+$arr = [];
 
-    if($con->ping()) {
-        $arr['connected'] = true;
+// checks if connection to the database is successful
+if($connection->ping()) {
+    $arr['connected'] = true;
+} else {
+    $arr['connected'] = false;
+}
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $arr['xid'] = $_POST['xid'];
+    $arr['name'] = $_POST['name'];
+    $arr['message'] = $_POST['message'];
+    $arr['action'] = $_POST['action'];
+} else {
+    $arr['xid'] = '222';
+    $arr['name'] = $_POST['noname'];
+    $arr['message'] = $_POST['nomessage'];
+    $arr['action'] = 'GET';
+}
+
+if (preg_match('/^[0-9]+$/', $arr['xid'])) {
+
+}
+
+if($arr['action'] === 'ADD') {
+    $sqla = $connection->prepare("SELECT xid FROM `pancakes` WHERE xid=?");
+    $requestsqla = $sqla->bind_param('i', $arr['xid']);
+    $sqla->execute();
+    $result = $sqla->get_result();
+
+    if($result->num_rows === 0) {
+        $sql = $connection->prepare("INSERT INTO `pancakes` (`name`, `message`, `xid`) VALUES (?,?,?);");
+        $requestsql = $sql->bind_param('ssi', $arr['name'], $arr['message'], $arr['xid']);
+        if($requestsql) { $sql->execute(); $arr['id'] = $connection->insert_id; } $arr['status'] = 'Added';
     } else {
-        $arr['connected'] = false;
+        $sqlb = $connection->prepare("UPDATE `pancakes` SET `name` = ?, `message` = ? WHERE `xid` = ?;");
+        $requestsqlb = $sqlb->bind_param('ssi', $arr['name'], $arr['message'], $arr['xid']);
+        if($requestsqlb) { $sqlb->execute(); $arr['status'] = 'Updated';}
     }
 
-
-
-    $arr['status'] = ['READY'];
-    
-    if($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $arr['name'] = $_POST['name'];
-        $arr['message'] = $_POST['message'];
-    } else {
-        $arr['name'] = 'anonymous';
-        $arr['message'] = 'NONE';
+if($arr['action'] === 'GET') {
+    $sqla = $connection->prepare("SELECT xid FROM `pancakes` WHERE xid=?");
+    $requestsqla = $sqla->bind_param('i', $arr['xid']);
+    $sqla->execute();
+    $result = $sqla->get_result();
+    $data = [];
+    if($result->num_row > 0) {
+        while($row=$result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        $arr['response'] = $data;
     }
-    echo json_encode($arr);
 
-    $sql = $con->prepare("INSERT INTO `pancakes` (`name`, `message`) VALUES (?,?)");
-    $reqsql = $sql->bind_param('ss', $arr['name'], $arr['message']);
-    if($reqsql) {
-        $sql->execute();
-        $arr['id'] = $con->insert_id;
-    }
+}
+
+
+} // ----- end of if statement
+
+echo json_encode($arr);
 
 ?>
--- script.js
+++ script.js
@@ -1,28 +1,15 @@
 $(document).ready(function() {
-    console.log('hi :]');
-    $('#postPancake').on('click', function() {
-        var vars = {
-            name: $('#name').val(),
-            message: $('#message').val()
-        }
-        $.ajax({
-            url:'api.php',
-            data: vars,
-            type:'POST'
-        })
-        .done(function(data) {
-            console.log(data);
-        })
-        .fail(function(xhr,textstatus) {
-            console.log(xhr);
-            console.log(textstatus);
-        })
-    })
 
-    // REQUEST PANCAKES BUTTON
-    $('#getPancake').on('click', function() {
+
+    // POST DATA BUTTON
+    $('#postData').on('click', function() {
+        console.log('POST data clicked');
+
         var vars = {
-            action: 'GET'
+            xid: $('#xid').val(),
+            name: $('#name').val(),
+            message: $('#message').val(),
+            action: 'ADD'
         }
         $.ajax({
             url:'api.php',
@@ -30,13 +17,39 @@ $(document).ready(function() {
             type:'POST',
             dataType: 'json'
         })
-        .done(function(data) {
-            console.log(data);
+        .done(function(result) {
+            console.log(result);
         })
         .fail(function(xhr,textstatus) {
             console.log(xhr);
             console.log(textstatus);
         })
-    })
-// END OF DOCUMENT.READY    
-});
+    }) // ----- END OF POST DATA BUTTON
+
+
+
+    // GET DATA BUTTON
+    $('#getData').on('click', function() {
+        console.log('GET data clicked');
+
+        var vars = {
+            xid: $('#xid').val(),
+            action: 'GET'
+        }
+        $.ajax({
+            url:'api.php',
+            type:'POST',
+            dataType:'json'
+        })
+        .done(function(result) {
+            console.log(result);
+        })
+        .fail(function(xhr,textstatus) {
+            console.log(xhr);
+            console.log(textstatus);
+        })
+    }) // ----- END OF REQUEST DATA BUTTON
+
+
+
+}); // ----- END OF DOCUMENT.READY    
